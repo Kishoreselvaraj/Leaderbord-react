e76e17f5ac5288911236988b5b1b161028477c20
Kishore Selvaraj <89259172+Kishoreselvaraj@users.noreply.github.com>
2024-07-18 09:35:26 +0530

    we

--- leaderboard/src/Carousel.tsx
+++ leaderboard/src/Carousel.tsx
@@ -6,7 +6,7 @@ const Carousel: React.FC = () => {
   return (
     <div className="carousel">
       <div className="carousel-track">
-        <div className="carousel-slide"><img src="" alt="Slide 1" /></div>
+        <div className="carousel-slide"><img src="leaderboard\src\img\KishoreProfile.jpg" alt="Slide 1" /></div>
         <div className="carousel-slide"><img src="" alt="Slide 2" /></div>
         <div className="carousel-slide"><img src="" alt="Slide 3" /></div>
         <div className="carousel-slide"><img src="" alt="Slide 4" /></div>
